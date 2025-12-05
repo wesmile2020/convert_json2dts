04c3ae33d6d4c792c93de2395904e2257296bd6e
lilei <1211921908@qq.com>
2025-12-05 11:14:20 +0800

    update: remove docs

--- docs/assets/typescript-UfA1CXbK.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./typescript-im2osBBS.js";export{t as conf,e as language};
--- docs/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class JSON2Dts {
-    transformByJSON(data: unknown): string;
-    transformByJSONString(code: string): string;
-    convertJSONToDts(data: unknown): string;
-    convertJSONStringToDts(code: string): string;
-}
-
-export declare const version: string;
